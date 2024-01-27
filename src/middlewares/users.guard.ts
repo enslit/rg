a6439eb59fa40a7bc45eb0b6d9d44ece2d68d18a
Alexander Emelyanov <Alexaneder.Emelyanov@kaspersky.com>
2024-01-27 21:16:12 +0300

    add Curly_Y

--- src/middlewares/users.guard.ts
+++ src/middlewares/users.guard.ts
@@ -3,6 +3,7 @@ import { Update } from 'telegraf/typings/core/types/typegram';
 
 const mapTelegramUsersToEmployees: Record<number, string> = {
   546865995: 'C7D4F9F6-52B6-4346-A4A2-BFAA8D7DCCCC', // Prosto_Alex -> Юлиана Пономарева
+  357936666: 'C7D4F9F6-52B6-4346-A4A2-BFAA8D7DCCCC', // Curly_Y 
 }
 
 export const usersGuard: MiddlewareFn<Context<Update>> = (ctx, next) => {
@@ -14,7 +15,6 @@ export const usersGuard: MiddlewareFn<Context<Update>> = (ctx, next) => {
   const employeeId = mapTelegramUsersToEmployees[ctx.from.id]
 
   if (!employeeId) {
-    console.log('UNKNOWN USER', ctx)
     ctx.reply("Unknown user");
     return;
   }
